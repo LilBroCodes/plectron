261764dd2727f7cd1ba2c5e176298bac8ea8141d
LilBroCodes <lilbrocodes@gmail.com>
2025-01-24 22:25:02 +0100

    Transfer commit, basic UI side setup.

---
+++ main.js
@@ -0,0 +1,30 @@
+import { app, BrowserWindow } from 'electron'
+import * as path from 'path'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const createWindow = () => {
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+    mainWindow.loadFile('src/index.html').then(() => {})
+}
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
